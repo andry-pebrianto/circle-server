4f361d7addb241e97c2c89b7741e8379669c8a1a
Andry Pebrianto <andrypeb227@gmail.com>
2023-11-29 14:49:12 +0700

    Add ssl true

--- database/data-source.ts
+++ database/data-source.ts
@@ -6,7 +6,7 @@ import { Thread } from "./entities/Thread";
 import { Reply } from "./entities/Reply";
 import { Like } from "./entities/Like";
 import { Upload } from "./entities/Upload";
-import { MigrationFile1698899930953 } from './migration/1698899930953-MigrationFile';
+import { MigrationFile1698899930953 } from "./migration/1698899930953-MigrationFile";
 
 export const PostgreDataSource = new DataSource({
   type: "postgres",
@@ -20,4 +20,5 @@ export const PostgreDataSource = new DataSource({
   entities: [User, Thread, Reply, Like, Upload],
   migrations: [MigrationFile1698899930953],
   subscribers: [],
+  ssl: true,
 });
